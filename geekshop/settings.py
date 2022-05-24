75ea6aa6874c0c6bcc4ff4c87881916e695fcb73
MadEngineAR <scorpio-84@mail.ru>
2022-05-24 13:29:39 +0500

    update lesson_2_6_REGRU update project2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -116,7 +116,7 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-SERVER = False
+SERVER = True
 if SERVER:
     DATABASES = {
         'default': {
